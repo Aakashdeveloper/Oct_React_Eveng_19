7586ac0b2e94a1a5e41ac44e52535e205ba2b4cf
aakashDeveloper <ahanda205@gmail.com>
2019-10-17 04:09:51 +0100

    a

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -2,12 +2,30 @@ import React,{Component} from 'react';
 import './Header.css'
 
 class Header extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            title:"React App",
+            userInput:"User text Here"
+        }
+    }
+
+    inputChanges(event){
+        console.log(event.target.value)
+        this.setState({userInput:event.target.value?event.target.value:'User text Here'})
+    }
+
     render(){
         return(
             <header>
-               <div className="logo">Logo</div>
+               <div className="logo"
+               onClick={()=>{console.log('logo clicked')}}>
+                   {this.state.title}
+               </div>
                <center>
-                <input/>
+                <input  onChange={this.inputChanges.bind(this)}/>
+                <h4>{this.state.userInput}</h4>
                </center>
                <hr/>
             </header>
